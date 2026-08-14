a8c3f63ab8f8cb94ea28a95a66772fdb435270ef
MuhamadZia <ziahk68@gmail.com>
2026-08-14 13:37:19 +0700

    Add recommendation engine

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,5 @@
+from services.trip_service import get_trip_category, recommend_places, get_travel_season, calculate_daily_budget
+
 def print_trip_summary(destination, country, days, budget, currency, travel_month):
     print("========================")
     print("KelanaAI")
@@ -6,8 +8,16 @@ def print_trip_summary(destination, country, days, budget, currency, travel_mont
     print(f"Country     : {country}")
     print(f"Days        : {days}")
     print(f"Budget      : {budget}")
+    print(f"Category    : {get_trip_category(budget)}")
     print(f"Currency    : {currency}")
+    print(f"Daily Budget: {f"{calculate_daily_budget(budget, days)} {currency}/Day"}")
     print(f"Travel Month: {travel_month}")
+    print(f"Season      : {get_travel_season(travel_month)}")
+
+    print("\n")
+    print("Recommended Places")
+    for item in recommend_places():
+        print(f"- {item}")
 
 # Call it with any trip
 destination = input("destination    : ")
---
+++ backend/services/trip_service.py
@@ -0,0 +1,26 @@
+
+def get_trip_category(budget:float) -> str:
+    if budget < 1000:
+        return "Backpacker"
+    elif budget <= 3000:
+        return "Standard"
+    else:
+        return "Luxury"
+    
+def get_travel_season(month:str) -> str:
+    if month.lower() == "december":
+        return "Peak Season"
+    elif month.lower() == "june":
+        return "Holiday Season"
+    else:
+        return "Regular Season"
+
+def calculate_daily_budget(budget:float, days:int) -> float:
+    return budget / days
+
+def recommend_places() -> list[str]:
+    return [
+        "Tokyo Tower",
+        "Shibuya",
+        "Mount Fuji"
+    ]
